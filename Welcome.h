57766f4c384d19e504f501ad9adcc766965c5e17
eCampusFall2022 <simranjit.singh@nscc.ca>
2023-09-12 12:26:06 +0000

    Commit 1

---
+++ Welcome.h
@@ -0,0 +1,14 @@
+//
+// Created by eCampusFall2022 on 9/12/2023.
+//
+
+#ifndef HELLOCPLUSPLUS_WELCOME_H
+#define HELLOCPLUSPLUS_WELCOME_H
+
+
+class Welcome {
+
+};
+
+
+#endif //HELLOCPLUSPLUS_WELCOME_H
